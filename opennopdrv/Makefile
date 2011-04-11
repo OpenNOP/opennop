13de6fd8c74ce7de287aea58e947399851a7825d
Justin Yaple <yaplej@gmail.com>
2011-04-11 05:53:33 +0000

    Separating the components into their own folders.

---
+++ opennopdrv/Makefile
@@ -0,0 +1,23 @@
+
+override MODULE = opennopdrv
+OBJS = module.o
+
+MODULE_EXT	:= ko
+$(MODULE)-y	:= $(OBJS)
+
+obj-m += $(MODULE).o
+
+KDIR    := /lib/modules/$(shell uname -r)/build
+PWD             := $(shell pwd)
+
+
+all modules:        
+	$(MAKE) -C $(KDIR) M=$(PWD) modules
+clean:        
+	$(MAKE) -C $(KDIR) M=$(PWD) clean
+	rm Module.symvers Module.markers
+	
+install:
+	install -d -v /usr/local/lib/modules/`uname -r`/misc/
+	install $(MODULE).ko /usr/local/lib/modules/`uname -r`/misc/$(MODULE).ko
+	
---
+++ opennopdrv/module.c
@@ -0,0 +1,161 @@
+#include <linux/module.h> // for all modules
+#include <linux/init.h> // for entry/exit macros
+#include <linux/kernel.h> // for printk priority macros
+#include <linux/ip.h> // for access to IP
+#include <linux/tcp.h>
+#include <linux/if_ether.h>	// for access to Ethernet
+#include <linux/inetdevice.h>
+#include <linux/netdevice.h>
+#include <linux/timer.h> // to monitor the health of the daemon
+
+
+#include <linux/skbuff.h> // for access to sk_buffs
+#include <linux/netfilter_ipv4.h> // for netfilter hooks
+
+#include <net/protocol.h>
+#include <net/tcp.h>
+#include <net/genetlink.h> // for generic netlink
+
+#define HBINTERVAL 5 /* Number of seconds in heartbeat interval. */
+
+/* Daemon state Definitions */
+#define DOWN 0
+#define UP 1
+
+/* Bool Definitions */
+#define FALSE 0
+#define TRUE 1
+
+/* Global Variables */
+int daemonstate = DOWN; /* Current state of the daemon. */
+int hbintervals = 3; /* Number of missed heartbeat intervals before Daemon is down. */
+struct timeval tv_lasthb; /* Last received "UP" heartbeat from Daemon. */
+struct timer_list daemonmonitor; /* Timer that checks the Daemon state. */
+
+/* Debug Variables */ 
+int DEBUG_HEARTBEAT = FALSE;
+
+#include "netlink.h"
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Justin Yaple <yaplej@gmail.com>");
+MODULE_DESCRIPTION("Intercepts network traffic and queues it to the OpenNOP daemon.");
+MODULE_ALIAS("opennopdrv");
+
+static void daemonmonitor_function(unsigned long data){
+	struct timeval tv_now; // current time.
+	int timespan;
+	timespan = hbintervals * HBINTERVAL;
+	do_gettimeofday(&tv_now); // Get the time from hardware.
+	
+	/*
+	 *  Check that the daemon has been sending heartbeats. 
+	 */
+	 if (tv_now.tv_sec - timespan >= tv_lasthb.tv_sec){
+	 	daemonstate = DOWN;
+	 	
+	 	if (DEBUG_HEARTBEAT == TRUE){
+	 		printk(KERN_ALERT "No heartbeat. timespan:%i now:%i, lasthb:%i \n",timespan, (unsigned int)tv_now.tv_sec, (unsigned int)tv_lasthb.tv_sec);
+	 	}
+	 }
+	 else{
+	 	daemonstate = UP;
+	 	
+	 	if (DEBUG_HEARTBEAT == TRUE){
+	 		printk(KERN_ALERT "Got a heartbeat.  timespan:%i now:%i, lasthb:%i \n",timespan, (unsigned int)tv_now.tv_sec, (unsigned int)tv_lasthb.tv_sec);
+	 	}
+	 }
+	 
+mod_timer(&daemonmonitor, jiffies + (hbintervals * HBINTERVAL) * HZ); // Update the timer for the next run.
+}
+
+static unsigned int
+queue_function(unsigned int hooknum, 
+struct sk_buff **skb, 
+const struct net_device *in, 
+const struct net_device *out, 
+int (*okfn)(struct sk_buff *)){
+	struct iphdr *iph = NULL;
+
+	/*
+	 * yaplej: Process the packet only,
+	 * if it was intended for this host,
+	 * is not NULL,
+	 * and is an IP packet.
+	 *
+	 * This limits processing packets to in-bound traffic only!
+	 */
+	if ((skb != NULL) && 
+		((*skb)->pkt_type == PACKET_HOST) && 
+		((*skb)->protocol == htons(ETH_P_IP))){
+		iph = ip_hdr((*skb)); // access ip header.
+
+		/*
+		 * yaple: Process only TCP segments.
+		 */
+		
+		if (iph->protocol == IPPROTO_TCP){
+			
+			/*
+			 * Check that the daemon is UP.
+			 */
+			 if (daemonstate == UP) {
+			 	return NF_QUEUE;
+			 }
+		}
+	}
+	
+	return NF_ACCEPT;
+}
+
+static struct nf_hook_ops opennop_hook = { // This is a netfilter hook.
+	.hook = queue_function, // Function that executes.
+	.hooknum = NF_IP_FORWARD, // For routed traffic only.
+	.pf = PF_INET, // Only for IP packets.
+	.priority = NF_IP_PRI_FIRST, // My hook executes first.
+};
+
+static __s32 __init 
+opennopdrv_init(void){
+	int err;
+	int timespan;
+	
+	timespan = hbintervals * HBINTERVAL;
+	
+	err = genl_register_family(&opennop_family);
+	if (err != 0){
+		return err;
+	}
+	err = genl_register_ops(&opennop_family, &opennop_ops_echo);
+		if (err != 0){
+		return err;
+	}
+	err = genl_register_ops(&opennop_family, &opennop_ops_hb);
+		if (err != 0){
+		return err;
+	}
+	
+	nf_register_hook(&opennop_hook);
+	
+	init_timer(&daemonmonitor);
+	daemonmonitor.expires = jiffies + (timespan * HZ);
+	daemonmonitor.data = 0;
+	daemonmonitor.function = daemonmonitor_function;
+	add_timer(&daemonmonitor);
+	return 0;
+	
+} 
+
+static void __exit 
+opennopdrv_exit(void){
+	
+	genl_unregister_family(&opennop_family);
+
+	nf_unregister_hook(&opennop_hook);
+	
+	del_timer_sync(&daemonmonitor);
+
+}
+
+module_init(opennopdrv_init);
+module_exit(opennopdrv_exit);
