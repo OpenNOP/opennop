6530fea16a51ae03460e34a75e637d79edb20bc9
Justin Yaple <yaplej@users.sourceforge.net>
2012-07-27 07:01:21 +0000

    Groundwork for cli/event system.

--- include/cli.h
+++ include/cli.h
@@ -3,10 +3,13 @@
 
 #define MSGSZ     128
 
-typedef struct msgbuf {
-         long    mtype;
-         char    mtext[MSGSZ];
- } message_buf;
+typedef struct msgbuf
+{
+    long	mtype; // Type 1 = to the Daemon, all others are the PID the message is going to.
+    pid_t	sender; // Who sent the message.
+    char	mtext[MSGSZ]; // The commant entered or returning message.
+}
+message_buf;
 
 void *cli_function (void *dummyPtr);
 
--- opennopd/subsystems/cli.c
+++ opennopd/subsystems/cli.c
@@ -16,101 +16,113 @@
 
 
 
-void *cli_function (void *dummyPtr){
-	int msqid;
+void *cli_function (void *dummyPtr)
+{
+    int msqid;
     int msgflg = IPC_CREAT | 0666;
     key_t key;
     message_buf rbuf;
+    message_buf sbuf;
     char message [LOGSZ];
-    
-	/*
-	 * Get the message queue id for the
-	 * "name" 1234, which was created by
-	 * the server.
-	 */
-	if ((key = ftok("/var/opennop",'o')) == -1) { /* Create the opennop key */
-		sprintf(message, "Could not create the opennop message queue key.");
+    size_t buf_length;
+    int stringcompare;
+
+    /*
+     * Get the message queue id for the
+     * "name" 1234, which was created by
+     * the server.
+     */
+    if ((key = ftok("/var/opennop",'o')) == -1)
+    { /* Create the opennop key */
+        sprintf(message, "Could not create the opennop message queue key.");
         logger(LOG_INFO, message);
         sprintf(message, "Does /var/opennop exist?");
         logger(LOG_INFO, message);
         exit(1);
-	}
-    
+    }
+
     sprintf(message, "\nmsgget: Calling msgget(%#x, %#o)\n", key, msgflg);
-	logger(LOG_INFO, message);
-	
-	if ((msqid = msgget(key, msgflg )) == -1) { /* Connect to the message queue */
+    logger(LOG_INFO, message);
+
+    if ((msqid = msgget(key, msgflg )) == -1)
+    { /* Connect to the message queue */
         sprintf(message, "Could not connect to the message queue.");
         logger(LOG_INFO, message);
         exit(1);
-    }else{
-		sprintf(message,"msgget: msgget succeeded: msqid = %d\n", msqid);
-		logger(LOG_INFO, message);
-	}
-	
-	while (servicestate >= STOPPING) {
-		
-		/*
-		 * This thread should run until the parent thread
-		 * has reached a "SHUTDOWN" state.
-		 * 
-		 * This is where the interface to send and receive 
-		 * command line events/messages should be processed.
-		 */
-		
-		
-		/*
-		 * Receive an answer of message type 1.
-		 */
-		if (msgrcv(msqid, &rbuf, MSGSZ, 1, 0) < 0) {
-			perror("msgrcv");
-			exit(1);
-		}
+    }
+    else
+    {
+        sprintf(message,"msgget: msgget succeeded: msqid = %d\n", msqid);
+        logger(LOG_INFO, message);
+    }
 
-		/*
-		 * Print the answer.
-		 */
-		sprintf(message,"%s\n", rbuf.mtext);
-		logger(LOG_INFO, message);
-		
-		
-		/*
-		 * This seems to be for sending a message.
-		 * I'll need to use this to reply back to the user interface.
-		 */
-		
-		//if (msgrcv(msqid, &sbuf, sizeof(sbuf.mtext), 0, 0) == -1) {
-       	// 	sprintf(message, "Error receiving message.");
-        //	logger(LOG_INFO, message);
-        //	exit(1);
-		//}else{
-		//	sprintf(message, sbuf.mtext);
-        //	logger(LOG_INFO, message);
-		//}			
-		
-		/*
-		 * 
-		 * This is for getting a message from the queue.
-		 
-		size_t buf_length;
-		sbuf.mtype = 1; //TWhen we use this it will be the PID of the CLI.
-		//fprintf(stderr,"msgget: msgget succeeded: msqid = %d\n", msqid);
-		strcpy(sbuf.mtext, "Did you get this?");
-    	//fprintf(stderr,"msgget: msgget succeeded: msqid = %d\n", msqid);
-		buf_length = strlen(sbuf.mtext) + 1 ;
-		
-		if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0){
-			//sprintf(message, "%i, %l, %s, %d\n", msqid, sbuf.mtype, sbuf.mtext, buf_length);
-			sprintf(message, "Error sending message.");
-			logger(LOG_INFO, message);
-    	}else{
-      		sprintf(message, "Message: \"%s\" Sent\n", sbuf.mtext);
-      		logger(LOG_INFO, message);
-		}
-		*/
-		
-		
-		
-	}
-	return NULL;
+    buf_length = sizeof(message_buf) - sizeof(long);
+
+    while (servicestate >= STOPPING)
+    {
+
+        /*
+         * This thread should run until the parent thread
+         * has reached a "SHUTDOWN" state.
+         * 
+         * This is where the interface to send and receive 
+         * command line events/messages should be processed.
+         */
+
+
+        /*
+         * Receive an answer of message type 1.
+         */
+        if (msgrcv(msqid, &rbuf, buf_length, 1, 0) < 0)
+        {
+            sprintf(message,"msgrcv");
+            logger(LOG_INFO, message);
+            exit(1);
+        }
+
+        /*
+         * Print the answer.
+         */
+        sprintf(message,"%i,%s\n",rbuf.sender, rbuf.mtext);
+        logger(LOG_INFO, message);
+
+
+        /*
+         * I want to know when the user enters "show" in the CLI.
+         * Then send an "OK" message back to the CLI.
+         * Ill need to develop a structure that will be used,
+         * to pass data back/forth this is started in the cli.h
+         * the name might change later.  The framework of how
+         * CLI command will be read is next.
+         */
+        stringcompare = strncmp(rbuf.mtext, "show", MSGSZ);
+        if (stringcompare == 0)
+        {
+
+            /*
+             * Send a message back to the CLI.
+             * I think this should be a function.
+             * It will be used a lot by different modules,
+             * to send output back to the CLI.
+             */
+
+            sbuf.mtype = rbuf.sender; //TWhen we use this it will be the PID of the CLI.
+            strcpy(sbuf.mtext, "OK\n");
+
+            if (msgsnd(msqid, &sbuf, buf_length, IPC_NOWAIT) < 0)
+            {
+                sprintf(message, "Error sending message.");
+                logger(LOG_INFO, message);
+            }
+            else
+            {
+                sprintf(message, "Message: \"%s\" Sent\n", sbuf.mtext);
+                logger(LOG_INFO, message);
+            }
+
+        }
+
+
+    }
+    return NULL;
 }
