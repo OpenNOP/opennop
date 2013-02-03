55e9a492a5a52acf85c46f5ec5ae1aeae9d26e3a
Justin Yaple <yaplej@gmail.com>
2013-02-03 20:22:48 +0000

    Added methods for queueing and dequeuing packets.

---
+++ opennopd/packet.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "../include/packet.h"
+
+struct packet *newpacket(void)
+{
+    struct packet *thispacket = NULL;
+
+    thispacket = calloc(1,sizeof(struct packet)); // Allocate space for a new packet.
+    thispacket->head = NULL;
+    thispacket->next = NULL;
+    thispacket->prev = NULL;
+
+    return thispacket;
+}
+
+int save_packet(struct packet *thispacket,struct nfq_q_handle *hq, u_int32_t id, int ret, __u8 *originalpacket, struct session *thissession)
+{
+	thispacket->hq = hq; // Save the queue handle.
+	thispacket->id = id; // Save this packets id.
+	memmove(thispacket->data, originalpacket, ret); // Save the packet.
+	
+	return 0;
+}
