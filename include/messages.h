6b53005ffc58211cfc918e40de852764af58a227
Justin Yaple <yaplej@users.sourceforge.net>
2013-03-14 19:53:20 +0000

    Formating code.

--- include/messages.h
+++ include/messages.h
@@ -3,13 +3,12 @@
 
 #define MSGSZ     128
 
-struct msgbuf
-{
-    long	mtype; // Type 1 = to the Daemon, all others are the PID the message is going to.
-    pid_t	sender; // Who sent the message.
-    char	mtext[MSGSZ]; // The commant entered or returning message.
+struct msgbuf {
+	long mtype; // Type 1 = to the Daemon, all others are the PID the message is going to.
+	pid_t sender; // Who sent the message.
+	char mtext[MSGSZ]; // The commant entered or returning message.
 };
 
-void *messages_function (void *dummyPtr);
+void *messages_function(void *dummyPtr);
 
 #endif /*MESSAGES_H_*/
