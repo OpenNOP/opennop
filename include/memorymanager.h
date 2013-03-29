54e2be375e1b1f3d14567c51909fa9c0342726df
Justin Yaple <yaplej@gmail.com>
2013-03-28 20:53:32 -0400

    Debug messages and fixed missing declaration.

--- include/memorymanager.h
+++ include/memorymanager.h
@@ -19,5 +19,7 @@ int allocatefreepacketbuffers(struct packet_head *queue, int bufferstoallocate);
 u_int32_t movepacketbuffers(struct packet_head *fromqueue,
 		struct packet_head *toqueue);
 
+struct packet *get_freepacket_buffer(void);
+
 int put_freepacket_buffer(struct packet *thispacket);
 #endif /*MEMORYMANAGER_H_*/
