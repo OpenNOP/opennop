0ae2b5ccf493370ab94ac69b6cf70cce6cc89418
Justin Yaple <yaplej@gmail.com>
2013-04-01 11:10:24 -0400

    Removing global variable for numworkers.

--- opennopd/subsystems/memorymanager.c
+++ opennopd/subsystems/memorymanager.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #include <pthread.h> // for multi-threading
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 
 #include <sys/types.h>
 #include <linux/types.h>
