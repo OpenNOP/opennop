568eea0a8936a07ddfc87cf5622c79211ec8fd6b
Justin Yaple <yaplej@users.sourceforge.net>
2013-04-01 13:51:17 -0400

    Removed global from tcpoptions.h.

--- include/tcpoptions.h
+++ include/tcpoptions.h
@@ -4,8 +4,6 @@
 
 #include <linux/types.h>
 
-extern int DEBUG_TCPOPTIONS;
-
 __u8 optlen(const __u8 *opt, __u8 offset);
 __u64 __get_tcp_option(__u8 *ippacket, __u8 tcpoptnum);
 int __set_tcp_option(__u8 *ippacket, unsigned int tcpoptnum,
