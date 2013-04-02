348dff3ebe30083988ae9b79d876f0db91947712
Justin Yaple <yaplej@gmail.com>
2013-04-02 00:52:07 -0400

    Moved queue management function to correct module.

--- include/queuemanager.h
+++ include/queuemanager.h
@@ -2,6 +2,7 @@
 #define QUEUEMANAGER_H_
 #define _GNU_SOURCE
 
+#include <sys/types.h>
 #include <linux/types.h>
 
 #include <libnetfilter_queue/libnetfilter_queue.h> // for access to Netfilter Queue
@@ -11,8 +12,11 @@
 
 extern int DEBUG_QUEUEMANAGER;
 
-//int queue_packet(struct nfq_q_handle *hq, u_int32_t id, int ret, __u8 *originalpacket, struct session *thissession);
 int queue_packet(struct packet_head *queue, struct packet *thispacket);
+
 struct packet *dequeue_packet(struct packet_head *queue, int signal);
+
+u_int32_t move_queued_packets(struct packet_head *fromqueue,
+		struct packet_head *toqueue);
 
 #endif /*QUEUEMANAGER_H_*/
