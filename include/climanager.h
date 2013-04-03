7e74a00f4f94d957a809427bac59e3d57127b101
Justin Yaple <yaplej@users.sourceforge.net>
2013-04-03 18:38:06 -0400

    Enhanced CLI to allow other modules to register commands.

--- include/climanager.h
+++ include/climanager.h
@@ -3,11 +3,13 @@
 
 #include "clicommands.h"
 
+#define	MAX_BUFFER_SIZE	1024
 
 int cli_process_message(int client_fd, char *buffer, int d_len);
 void start_cli_server();
 int cli_help();
 void cli_manager_init();
+int cli_send_feedback(char *msg);
 
 
 #endif
