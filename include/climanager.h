309311991f6350c004c517a0cfd2eff721475bd8
Justin Yaple <yaplej@users.sourceforge.net>
2013-05-18 10:53:55 -0400

    Improved cli to handle multiple sessions.

--- include/climanager.h
+++ include/climanager.h
@@ -2,14 +2,14 @@
 #define	_CLI_MANAGER_H_
 
 #include "clicommands.h"
-
-#define	MAX_BUFFER_SIZE	1024
+#include "clisocket.h"
 
 int cli_process_message(int client_fd, char *buffer, int d_len);
 void start_cli_server();
 int cli_help();
 void cli_manager_init();
-int cli_send_feedback(char *msg);
+int cli_send_feedback(int client_fd, char *msg);
 void bytestostringbps(char *output, __u32 count);
+void *client_handler(void *socket_desc);
 
 #endif
