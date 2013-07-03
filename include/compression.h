8bc5c73b2aabd1ee3675af9ca410df6ed1d3b127
Justin Yaple <yaplej@gmail.com>
2013-07-02 21:25:40 -0400

    Parsing and storing cli commands in command tree.

--- include/compression.h
+++ include/compression.h
@@ -6,9 +6,9 @@
 
 #include "quicklz.h"
 
-int cli_show_compression(int client_fd);
-int cli_compression_enable(int client_fd);
-int cli_compression_disable(int client_fd);
+int cli_show_compression(int client_fd, char *args);
+int cli_compression_enable(int client_fd, char *args);
+int cli_compression_disable(int client_fd, char *args);
 unsigned int tcp_compress(__u8 *ippacket, __u8 *lzbuffer, qlz_state_compress *state_compress);
 unsigned int tcp_decompress(__u8 *ippacket, __u8 *lzbuffer, qlz_state_decompress *state_decompress);
 
