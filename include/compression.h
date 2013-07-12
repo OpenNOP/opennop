d83a1b626b96da535e07632281f3ca660c48404b
Justin Yaple <yaplej@users.sourceforge.net>
2013-07-12 16:44:03 -0400

    Updated to new cli function and standardized cli prompt

--- include/compression.h
+++ include/compression.h
@@ -6,9 +6,9 @@
 
 #include "quicklz.h"
 
-int cli_show_compression(int client_fd, char *args);
-int cli_compression_enable(int client_fd, char *args);
-int cli_compression_disable(int client_fd, char *args);
+int cli_show_compression(int client_fd, char **parameters, int numparameters);
+int cli_compression_enable(int client_fd, char **parameters, int numparameters);
+int cli_compression_disable(int client_fd, char **parameters, int numparameters);
 unsigned int tcp_compress(__u8 *ippacket, __u8 *lzbuffer, qlz_state_compress *state_compress);
 unsigned int tcp_decompress(__u8 *ippacket, __u8 *lzbuffer, qlz_state_decompress *state_decompress);
 
