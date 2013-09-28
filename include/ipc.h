2c4e4b9b60c9b4b8afc30a1393d3655d4efbe816
Justin Yaple <yaplej@gmail.com>
2013-09-28 17:27:53 -0400

    Added data validation for node input.

--- include/ipc.h
+++ include/ipc.h
@@ -18,7 +18,7 @@ void start_ipc();
 int cli_node(int client_fd, char **parameters, int numparameters);
 int cli_no_node(int client_fd, char **parameters, int numparameters);
 int cli_show_nodes(int client_fd, char **parameters, int numparameters);
-int add_update_node(int client_fd, char *stringip, char *key);
+int validate_node_input(int client_fd, char *stringip, char *key);
 int del_node(int client_fd, char *stringip);
 int cli_node_help(int client_fd);
 
