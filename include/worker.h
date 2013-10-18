76a3e43c03de230a3eba08c7f7fad514750981e8
Justin Yaple <yaplej@gmail.com>
2013-10-18 13:30:54 -0400

    Add basic support for context|mode in the CLI.

--- include/worker.h
+++ include/worker.h
@@ -17,36 +17,36 @@
 
 #define MAXWORKERS 255 // Maximum number of workers to process packets.
 struct workercounters {
-	/*
-	 * number of packets processed.
-	 * used for pps calculation. can roll.
-	 * should increment at end of main loop after each packet is finished processing.
-	 */
-	__u32 packets;
-	__u32 packetsprevious;
-	__u32 pps; // Where the calculated pps are stored.
+    /*
+     * number of packets processed.
+     * used for pps calculation. can roll.
+     * should increment at end of main loop after each packet is finished processing.
+     */
+    __u32 packets;
+    __u32 packetsprevious;
+    __u32 pps; // Where the calculated pps are stored.
 
-	/*
-	 * number of bytes entering process.
-	 * used for bps calculation. can roll.
-	 * should increment as beginning of main loop after packet is received from queue.
-	 */
-	__u32 bytesin;
-	__u32 bytesinprevious;
-	__u32 bpsin; // Where the calculated bps are stored.
-	__u32 bytesout;
-	__u32 bytesoutprevious;
-	__u32 bpsout; // Where the calculated bps are stored.
+    /*
+     * number of bytes entering process.
+     * used for bps calculation. can roll.
+     * should increment as beginning of main loop after packet is received from queue.
+     */
+    __u32 bytesin;
+    __u32 bytesinprevious;
+    __u32 bpsin; // Where the calculated bps are stored.
+    __u32 bytesout;
+    __u32 bytesoutprevious;
+    __u32 bpsout; // Where the calculated bps are stored.
 
-	/*
-	 * Stores when the counters were last updated.
-	 */
-	struct timeval updated;
+    /*
+     * Stores when the counters were last updated.
+     */
+    struct timeval updated;
 
-	/*
-	 * When something reads/writes to this counter it should use the lock.
-	 */
-	pthread_mutex_t lock;
+    /*
+     * When something reads/writes to this counter it should use the lock.
+     */
+    pthread_mutex_t lock;
 };
 
 /*
@@ -56,19 +56,19 @@ struct workercounters {
  * and the buffer used for QuickLZ.
  */
 struct processor {
-	pthread_t t_processor;
-	int state; // Marks this thread as active. 1=running, 0=stopping, -1=stopped.
-	struct workercounters metrics;
-	struct packet_head queue;
-	__u8 *lzbuffer; // Buffer used for QuickLZ.
+    pthread_t t_processor;
+    int state; // Marks this thread as active. 1=running, 0=stopping, -1=stopped.
+    struct workercounters metrics;
+    struct packet_head queue;
+    __u8 *lzbuffer; // Buffer used for QuickLZ.
 };
 
 /* Structure contains the worker threads, queue, and status. */
 struct worker {
-	struct processor optimization; //Thread that will do all optimizations(input).  Coming from LAN.
-	struct processor deoptimization; //Thread that will undo optimizations(output).  Coming from WAN.
-	u_int32_t sessions; // Number of sessions assigned to the worker.
-	pthread_mutex_t lock; // Lock for this worker when adding sessions.
+    struct processor optimization; //Thread that will do all optimizations(input).  Coming from LAN.
+    struct processor deoptimization; //Thread that will undo optimizations(output).  Coming from WAN.
+    u_int32_t sessions; // Number of sessions assigned to the worker.
+    pthread_mutex_t lock; // Lock for this worker when adding sessions.
 };
 
 void *worker_thread(void *dummyPtr);
@@ -86,7 +86,7 @@ void decrement_worker_sessions(int i);
 int optimize_packet(__u8 queue, struct packet *thispacket);
 int deoptimize_packet(__u8 queue, struct packet *thispacket);
 void shutdown_workers();
-int cli_show_workers(int client_fd, char **parameters, int numparameters);
+struct commandresult cli_show_workers(int client_fd, char **parameters, int numparameters, void *data);
 void counter_updateworkermetrics(t_counterdata metric);
 struct session *closingsession(struct tcphdr *tcph, struct session *thissession);
 
