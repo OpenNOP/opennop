19357f85dd71412137ae5502dda4afb243b8f653
Justin Yaple <yaplej@gmail.com>
2014-06-03 10:39:23 -0400

    Using epoll server callback to handle data.

--- include/sockets.h
+++ include/sockets.h
@@ -6,15 +6,15 @@
 #include <sys/time.h>
 #include <sys/socket.h>
 
+typedef int (*t_epoll_callback)(int fd, void *buf);
+
 struct epoll_server {
 	int socket;
 	int epoll_fd;
 	struct epoll_event event;
 	struct epoll_event *events;
-	int (*callback)(void);
+	t_epoll_callback callback;
 };
-
-typedef int (*t_epoll_callback)(void);
 
 int new_ip_client(__u32 serverip ,int port);
 int new_ip_server(int port);
@@ -24,7 +24,7 @@ int new_unix_server(char* path);
 int accept_unix_client(int server_socket);
 int make_socket_non_blocking (int socket);
 int register_socket(int listener_socket, int epoll_fd, struct epoll_event *event);
-int new_ip_epoll_server(struct epoll_server *server, int (*callback)(void), int port);
+int new_ip_epoll_server(struct epoll_server *server, t_epoll_callback callback, int port);
 int shutdown_epoll_server(struct epoll_server *server);
 int epoll_handler(struct epoll_server *server);
 
