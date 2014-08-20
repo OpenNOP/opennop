fc1dc477a27c6d0ee63eb621dc76087c844dc52f
Justin Yaple <yaplej@gmail.com>
2014-08-20 14:44:09 -0400

    Added a better logging system to cleanup messages.

--- opennopd/logger.c
+++ opennopd/logger.c
@@ -10,6 +10,9 @@
 /*
  * Logs a message to either the screen or to syslog.
  */
+
+int LOGGING_LEVEL	=	LOGGING_WARN;	// Default log everything up to WARN messages (regardless of individual component setting).
+
 void logger(int LOG_TYPE, char *message)
 {
 	if (isdaemon == true){
@@ -18,4 +21,21 @@ void logger(int LOG_TYPE, char *message)
 	else{
 		printf(message);
 	}	
+}
+
+/** @brief Write a log message.
+ *
+ * We check to see what level of logging is enabled.
+ * Then check if any particular logging was enabled.
+ *
+ * @param level [in] The log level for this message.
+ * @param debug [in] The current debug level for the component sending the message.
+ * @return int
+ */
+int logger2(int level, int debug, char *message) {
+
+	if((level <= LOGGING_LEVEL) || ((debug & level) == level)) {
+		logger(LOG_INFO,message);
+	}
+	return 0;
 }
