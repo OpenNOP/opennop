d983ab481e710a105f0970d569f339bdb907430b
Justin Yaple <yaplej@gmail.com>
2014-12-09 14:17:42 -0500

    Implementing custom TCP Option for auto-detection to use UUID.

--- include/tcpoptions.h
+++ include/tcpoptions.h
@@ -4,9 +4,20 @@
 
 #include <linux/types.h>
 
+#define ONOP "ONOP"
+
+struct nodhdr {
+	__u8  length;
+	__u8  idlen:2,
+	      len:6;
+	__u8  iddata;
+	__u8  *data;
+};
+
 __u8 optlen(const __u8 *opt, __u8 offset);
 __u64 __get_tcp_option(__u8 *ippacket, __u8 tcpoptnum);
 int __set_tcp_option(__u8 *ippacket, unsigned int tcpoptnum,
 unsigned int tcpoptlen, u_int64_t tcpoptdata);
- 
+struct nodhdr *set_nod_header(__u8 *ippacket, const char *id);
+struct nodhdr *get_nod_header(__u8 *ippacket, const char *id);
 #endif /*TCPOPTIONS_H_*/
