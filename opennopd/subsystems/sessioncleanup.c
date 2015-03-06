008c46c7390e2122900fad485366bbdf4a94ef71
Justin Yaple <yaplej@gmail.com>
2015-03-06 17:32:19 -0500

    Put dead session detection timer back to 5 minutes.

--- opennopd/subsystems/sessioncleanup.c
+++ opennopd/subsystems/sessioncleanup.c
@@ -22,7 +22,7 @@
 
 static int rawsock = 0; // Used to send keep-alive messages.
 static int dead_session_detection = true; //Detect dead sessions by default.
-static int cleanup_timer = 30; // Time in seconds the dead session detection should run.
+static int cleanup_timer = 300; // Time in seconds the dead session detection should run.
 
 static int DEBUG_SESSION_TRACKING = LOGGING_INFO;
 
