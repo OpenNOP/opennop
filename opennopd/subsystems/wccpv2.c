51888931badb5709972fd15c7c2bd4e536214bcc
Justin Yaple <yaplej@gmail.com>
2015-04-24 22:07:39 -0400

    Fixed some compile warnings.

--- opennopd/subsystems/wccpv2.c
+++ opennopd/subsystems/wccpv2.c
@@ -227,7 +227,7 @@ void *wccp_thread(void *dummyPtr) {
 }
 
 void init_wccp(){
-	sprintf(&cli_wccp_mode.prompt, "opennopd->wccp# ");
+	sprintf(cli_wccp_mode.prompt, "opennopd->wccp# ");
     register_command(NULL, "wccp", cli_enter_wccp_mode, true, true);
     register_command(&cli_wccp_mode, "server", cli_wccp_server, true, false);
     register_command(&cli_wccp_mode, "password", cli_wccp_password, true, false);
