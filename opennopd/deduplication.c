5806ef778551edb9a8eba23b3bd607418956a14a
Justin Yaple <yaplej@gmail.com>
2015-08-27 15:16:01 -0400

    Fixed switch statement.

--- opennopd/deduplication.c
+++ opennopd/deduplication.c
@@ -25,6 +25,7 @@ struct hash{
 
 #define	DATABASE "/var/opennop/dedup.db"
 #define BLOCKS "blocks"
+#define MAXBLOCKS 16
 
 /** @brief Calculate hash values
  *
@@ -36,10 +37,10 @@ struct hash{
 int deduplicate(__u8 *ippacket, DB **dbp){
 	//* @todo Don't copy the data here.  No point.
 	//struct block blocks[12] = {0}; // 12 * 128 byte blocks = 1536 large enough for any standard size IP packet.
-	struct hash hashes[12]; // hash values for each block
+	struct hash hashes[MAXBLOCKS]; // hash values for each block
 	struct iphdr *iph = NULL;
 	struct tcphdr *tcph = NULL;
-	struct block *blockdata = NULL;
+	struct block *tcpdatablock = NULL;
 	__u16 datasize = 0;
 	__u8  numblocks = 0;
 	int i = 0;
@@ -50,48 +51,46 @@ int deduplicate(__u8 *ippacket, DB **dbp){
 		iph = (struct iphdr *) ippacket; // Access ip header.
 
 		if (iph->protocol == IPPROTO_TCP) { // If this is not a TCP segment abort compression.
-					tcph = (struct tcphdr *) (((u_int32_t *) ippacket) + iph->ihl);
+			tcph = (struct tcphdr *) (((u_int32_t *) ippacket) + iph->ihl);
 
-					datasize = (__u16)(ntohs(iph->tot_len) - iph->ihl * 4) - tcph->doff * 4;
-					blockdata = (__u8 *) tcph + tcph->doff * 4; // Find starting location of the TCP data.
+			datasize = (__u16)(ntohs(iph->tot_len) - iph->ihl * 4) - tcph->doff * 4;
+			tcpdatablock = (__u8 *) tcph + tcph->doff * 4; // Find starting location of the TCP data.
 
-					numblocks = datasize / 128;
+			numblocks = datasize / 128;
 
-					if((datasize % 128) != 0){
-						numblocks = numblocks + 1;
+			if((datasize % 128) != 0){
+				numblocks = numblocks + 1;
+			}
+
+			memset(&hashes, 0, sizeof(struct hash)*MAXBLOCKS);
+
+			if(numblocks < MAXBLOCKS){
+
+				for(i=0;i<numblocks;i++){
+					SHA512((unsigned char*)tcpdatablock[i].data, 128, (unsigned char *)&hashes[i]);
+
+					memset(&key, 0, sizeof(key));
+					memset(&data, 0, sizeof(data));
+
+					memcpy(&key.data, &hashes[i], sizeof(struct hash));
+					key.size = sizeof(struct hash);
+
+					memcpy(&data.data, &tcpdatablock[i].data, sizeof(struct block));
+					data.size = sizeof(struct block);
+
+					switch (ret = (*dbp)->put(*dbp, NULL, &key, &data, DB_NOOVERWRITE)){
+						case 0:
+							break;
+						case DB_KEYEXIST:
+							logger2(LOGGING_DEBUG,LOGGING_DEBUG,"[DEDUP] Duplicate block.\n");
+							break;
+						default:
+							logger2(LOGGING_DEBUG,LOGGING_DEBUG,"[DEDUP] Unknown error!\n");
+							exit(1);
 					}
-
-					memset(&hashes, 12, sizeof(struct hash));
-
-					if(numblocks < 12){
-
-						for(i=0;i<numblocks;i++){
-							SHA512((unsigned char*)blockdata[i].data, 128, (unsigned char *)&hashes[i]);
-
-							memset(&key, 0, sizeof(key));
-							memset(&data, 0, sizeof(data));
-
-							memcpy(&key.data, &hashes[i], sizeof(struct hash));
-							key.size = sizeof(struct hash);
-
-							memcpy(&data.data, &blockdata[i].data, sizeof(struct block));
-							data.size = sizeof(struct block);
-
-							switch ((ret = (*dbp)->put(*dbp, NULL, &key, &data, DB_NOOVERWRITE)) == 0){
-
-							case 0:
-								break;
-							case DB_KEYEXIST:
-								break;
-							default:
-								logger2(LOGGING_DEBUG,LOGGING_DEBUG,"[DEDUP] Unknown error!\n");
-								exit(1);
-							}
-
-						}
-					}
+				}
+			}
 		}
-
 	}
 
 	return 0;
