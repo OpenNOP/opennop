0d1aebfa7e425e856725a96507222337dd45290c
Justin Yaple <yaplej@gmail.com>
2016-04-09 09:00:39 -0400

    Don't assign value to "return".

--- opennopdrv/module.c
+++ opennopdrv/module.c
@@ -168,7 +168,7 @@ static int opennopdrv_init(void){
 	
 	#if (LINUX_VERSION_CODE >= KERNEL_VERSION (3, 0, 0))
 		printk(KERN_ALERT "[OpenNOPDrv]: Kernel Version >= 3.0.0 \n");
-		return = genl_register_family_with_ops(&opennop_nl_family, (struct genl_ops *)opennop_nl_ops, ARRAY_SIZE(opennop_nl_ops));
+		return genl_register_family_with_ops(&opennop_nl_family, (struct genl_ops *)opennop_nl_ops, ARRAY_SIZE(opennop_nl_ops));
 
 		if (err != 0){
 			return err;
